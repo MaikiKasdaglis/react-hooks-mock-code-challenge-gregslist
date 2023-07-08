076b04b1f1e2d1c1a726aa73ff94e324749d324b
Michael Kasdaglis <maikikasdaglis@gmail.com>
2023-07-08 15:54:29 -0600

    finishes lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+const [itemList, setItemList] = useState([])
+  useEffect(() => {
+    fetch(`http://localhost:6001/listings`)
+    .then(r => r.json())
+    .then(d => setItemList(d))
+  },[])
+
+
+  function removeItem(id){
+    console.log(`from listCart`, id)
+    const updated = itemList.filter((item) => item.id !== id)
+    setItemList(updated)
+
+    fetch(`http://localhost:6001/listings/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  }
+
+  function filterList (search) {
+   const newNew = itemList.filter((item) => item.description.includes(search))
+   setItemList(newNew)
+  }
+
+  
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+        <Header filterList={filterList}/>
+      <ListingsContainer removeItem={removeItem} itemList={itemList} />
     </div>
   );
 }
